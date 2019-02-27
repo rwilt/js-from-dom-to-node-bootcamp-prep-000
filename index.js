993c7f4d5bfb4e325762e4b3b3c62b5bf115a30c
rosie w <rosie.wilt@gmail.com>
2019-02-27 03:33:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 // write your code below!
-<<<<<<< HEAD
-console.log("Hello, World!")
-=======
-console.log("Hello, World!")
->>>>>>> be883857efbe7db5bff6c4bafb2b46df047f359e
+console.log('Hello, World!');
